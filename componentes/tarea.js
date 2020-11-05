a43df55ce54278cb51449e59c030f3ad90b27cec
stephym <stephymelo99@gmail.com>
2020-11-04 19:47:41 -0500

    fecha

--- componentes/tarea.js
+++ componentes/tarea.js
@@ -1,42 +1,88 @@
 const database2 = firebase.database();
+let fecha;
+var fechaSplit=[];
+
+
+
 
 
 class Tarea{
     constructor(tarealista){
         this.tarealista=tarealista;
+       
     }
     
 
+  
+   
+
     render=()=>{
-        var hora = Data.now() 
+        this.fecha=new Date();
+        this.fechaSplit=this.fecha.toString().split(" ");
+
+     
         let component = document.createElement('div');
         component.className="comp";
+        component.setAttribute("draggable","true");
         
 
         let tareaCont = document.createElement('div');
         tareaCont.className="tareaCont"
+        
         tareaCont.innerHTML = (
             this.tarealista.i
         );
+
+        let fechaCont = document.createElement('div');
+        fechaCont.className="fehcaCont";
+        fechaCont.innerHTML=this.fechaSplit[1]+" "+this.fechaSplit[2]+" "+this.fechaSplit[3];
+
+
         let deleteBtn = document.createElement('button');
         deleteBtn.className = "deleteBtn";
-        deleteBtn.innerHTML = "X";
+        deleteBtn.innerHTML = "x";
+
+        let moveRight = document.createElement('button');
+        moveRight.className = "moveRight";
+        moveRight.innerHTML = ">";
+
+        let moveLeft = document.createElement('button');
+        moveLeft.className = "moveLeft";
+        moveLeft.innerHTML = "<";
+
+        let divButtons=document.createElement('div');
+        divButtons.className="divButtons";
+
+
 
       
 
-       
-       
-
-        component.appendChild(tareaCont);
-        component.appendChild(deleteBtn);
-
+        
+   
         deleteBtn.addEventListener('click', ()=>{
+           
             database2.ref('tarea/tareaNueva/'+this.tarealista.id).set(null);
+            });
             
-        });
+
+
+        component.appendChild(fechaCont);
+        component.appendChild(tareaCont);
+        divButtons.appendChild(deleteBtn);
+        divButtons.appendChild(moveLeft);
+        divButtons.appendChild(moveRight);
+        component.appendChild(divButtons);
+     
 
         return component;
+
+        
     }
+
+
+
+
+   
 
 
     
--- js/logica.js
+++ js/logica.js
@@ -2,8 +2,9 @@ const inputTarea=document.getElementById('input');
 const buttonInput=document.getElementById('button');
 const database = firebase.database();
 const todo = document.getElementById('todoTareas');
+const doingTareas = document.getElementById('doingTareas');
 
-
+var dragged;
 
 
 ponerTarea = () => {
@@ -19,15 +20,12 @@ ponerTarea = () => {
     
         id:referencia.key,
         i : inputTarea.value,
-       
-
 
     };
     referencia.set(nuevaTareita);
     inputTarea.value = '';
-    
-
 }
+
 buttonInput.addEventListener('click',ponerTarea);
 
 //Lectura
@@ -41,6 +39,82 @@ database.ref('tarea/tareaNueva').on('value',function(data){
         }
     )
 });
+
+
+
+
+
+
+document.addEventListener("dragstart", function( event ) {
+   
+    dragged = event.target;
+  
+}, false);
+
+document.addEventListener("dragover", function( event ) {
+    event.preventDefault();
+}, false);
+
+document.addEventListener("drop", function( event ) {
+        
+    event.preventDefault();
+    
+    if ( event.target.id == "doingTareas") {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild( dragged );
+        event.target.appendChild( dragged );
+
+   }
+
+   
+    if ( event.target.id == "todoTareas" ) {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild( dragged );
+        event.target.appendChild( dragged );
+        todo.removeChild(dragged);      
+
+    }
+
+    if ( event.target.id == "doneTareas" ) {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild( dragged );
+        event.target.appendChild( dragged );
+         
+    }
+    
+   
+
+
+
+  
+}, false);
+
+
+
+
+/*
+document.addEventListener("dragenter", function( event ) {
+    // highlight potential drop target when the draggable element enters it
+    if ( event.target.className == "doingTareas" ) {
+        event.target.style.background = "purple";
+    }
+
+}, false);
+
+document.addEventListener("dragend", function( event ) {
+    // reset the transparency
+    event.target.style.opacity = "";
+}, false);
+
+
+document.addEventListener("dragleave", function( event ) {
+    if ( event.target.id == "doingTareas" ) {
+        event.target.style.background = "";
+      
+    }
+
+}, false);*/
+
 
 
 
